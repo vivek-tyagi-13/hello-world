1be0ffef50acefc1cb9fdb4f90e30629d1a7b2c4
Vivek Tyagi <vivtyagi0@publicisgroupe.net>
2020-12-19 13:35:58 +0530

    first commit

---
+++ src/main/java/com/example/vivek/HelloWorldApplication.java
@@ -0,0 +1,21 @@
+package com.example.vivek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+	
+	@RequestMapping("/hello")
+	public String getHello() {
+		return "hello";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
